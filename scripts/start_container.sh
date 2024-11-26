56c78ce95c75e7897b67fbd11603c09d56fc51c5
murtysiri <murtysiri@gmail.com>
2024-11-26 11:40:56 +0530

    first commit

---
+++ scripts/start_container.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Pull the Docker image from Docker Hub
+docker pull sreedhar8897/hotel-app:latest
+
+# Run the Docker image as a container
+docker run -dit -p 80:80 sreedhar8897/hotel-app
+
